b9a2d3198c033e439b5955912a0b184cd9cb41b5
Jeroen Benckhuijsen <jeroen.benckhuijsen@group9.nl>
2024-02-18 18:24:14 +0100

    New plauyer map and some drawing logic

--- src/ui/canvas.ts
+++ src/ui/canvas.ts
@@ -5,16 +5,86 @@ import {Dice} from '../game/dice';
 import gamemap from "./game-map.png";
 import {Turn} from "../game/turn";
 import {DiceSprite} from "./diceSprite";
+import {PlayerColor, PlayerSprite} from "./playerSprite";
 
 export class Canvas implements GameUI {
+
+    static NUMBER_OF_PLAYERS = Object.keys(PlayerColor).length / 2
+    static SQUARE_LOCATIONS : Array<Array<number>> = [
+        [0, ],
+        [1, ],
+        [2, ],
+        [3, ],
+        [4, ],
+        [5, ],
+        [6, ],
+        [7, ],
+        [8, ],
+        [9, ],
+        [10, ],
+        [11, ],
+        [12, ],
+        [13, ],
+        [14, ],
+        [15, ],
+        [16, ],
+        [17, ],
+        [18, ],
+        [19, ],
+        [20, ],
+        [21, ],
+        [22, ],
+        [23, ],
+        [24, ],
+        [25, ],
+        [26, ],
+        [27, ],
+        [28, ],
+        [29, ],
+        [30, ],
+        [31, ],
+        [32, ],
+        [33, ],
+        [34, ],
+        [35, ],
+        [36, ],
+        [37, ],
+        [38, ],
+        [39, ],
+        [40, ],
+        [41, ],
+        [42, ],
+        [43, ],
+        [44, ],
+        [45, ],
+        [46, ],
+        [47, ],
+        [48, ],
+        [49, ],
+        [50, ],
+        [51, ],
+        [52, ],
+        [53, ],
+        [54, ],
+        [55, ],
+        [56, ],
+        [57, ],
+        [58, ],
+        [59, ],
+        [60, ],
+        [61, ],
+        [62, ],
+        [63, ],
+    ]
     private myp5: p5
 
     // Game data
-    private game = new Game(this, 4)
+    private game = new Game(this, Canvas.NUMBER_OF_PLAYERS)
     private turn: Turn
 
     // UI Elements
     private dies: Array<DiceSprite> = new Array<DiceSprite>()
+    private players: Array<PlayerSprite> = new Array<PlayerSprite>()
     private rollButton: p5.Element
 
     // Animation state
@@ -29,11 +99,16 @@ export class Canvas implements GameUI {
 
     }
 
+    private static squarePosition(pos: number): Array<number> {
+        return Canvas.SQUARE_LOCATIONS[pos].slice(1)
+    }
+
     private setup(): void {
         this.myp5 = new p5((sketch) => {
 
             sketch.preload = () => {
                 DiceSprite.preload(sketch)
+                PlayerSprite.preload(sketch)
             }
 
             sketch.setup = () => {
@@ -42,9 +117,17 @@ export class Canvas implements GameUI {
                 sketch.createCanvas(sketch.windowWidth, sketch.windowHeight)
                 sketch.rectMode(sketch.CENTER).noFill().frameRate(30);
 
+                let mapImg = sketch.createImg(gamemap).position(0, 0)
+                mapImg.elt.className = 'gamemap'
+
                 for (let i = 0; i < 6; i++) {
                     this.dies.push(new DiceSprite(i + 1, 10, 40))
                 }
+
+                for (let i = 0; i < Canvas.NUMBER_OF_PLAYERS; i++) {
+                    this.players.push(new PlayerSprite(i))
+                }
+
                 this.rollButton = sketch.createButton("roll")
                     .position(10, 70)
                     .mouseClicked(() => {
@@ -52,7 +135,7 @@ export class Canvas implements GameUI {
 
                         // TODO: Test code -->
                         let dice = new Dice()
-                        this.turn = new Turn(dice.roll(), 0, 10)
+                        this.turn = new Turn(dice.roll(), 0, [10])
                         // remove once game logic is implemented <--
 
                         this.dies[this.turn.diceValue - 1].reset()
@@ -61,8 +144,6 @@ export class Canvas implements GameUI {
                         this.rollButton.elt.disabled = true
                     })
 
-                let mapImg = sketch.createImg(gamemap).position(100, 10)
-                mapImg.elt.className = 'gamemap'
             }
 
             sketch.windowResized = ()  => {
@@ -84,7 +165,13 @@ export class Canvas implements GameUI {
                     this.rollButton.elt.disabled = false
                 }
 
+                this.game.players.forEach((player, index) => {
+                    let pos = this.game.board.playerPosition(player)
+                    let coord = Canvas.squarePosition(pos)
 
+                    // TODO Check for moving of current player
+                    this.players[index].drawAt(coord[0], coord[1])
+                })
             }
         })
     }
