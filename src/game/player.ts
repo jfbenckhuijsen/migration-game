1ac1b92bbc6f8d003a93e956f0d86beb34b0d57e
Jeroen Benckhuijsen <jeroen.benckhuijsen@group9.nl>
2024-02-18 15:50:46 +0100

    Added wallet for the player

--- src/game/player.ts
+++ src/game/player.ts
@@ -3,8 +3,13 @@ import {Turn} from "./turn";
 import {Dice} from "./dice";
 
 export class Player {
+
+    static INITIAL_MONEY = 30
+    static MONEY_PER_STEP = 5
+
     id: number
     winner: boolean = false
+    money: number = Player.INITIAL_MONEY
 
     /**
      * Laat de huidige speler een beurt spelen door te gooien met de dobbelsteen. Daarna moet de speler naar het juiste
@@ -15,6 +20,8 @@ export class Player {
     takeTurn(board: Board) : Turn {
         var dice = new Dice()
         var steps = dice.roll()
+        this.money += steps * Player.MONEY_PER_STEP
+
         var oldPos = board.playerPosition(this)
         var newPos = board.movePlayer(this, steps)
         return new Turn(steps, oldPos, newPos);
