4d1e7e9bb48d1fabd29280784b8a1b30e509443a
Jeroen Benckhuijsen <jeroen.benckhuijsen@group9.nl>
2024-02-21 23:23:05 +0100

    Lint fixes

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,7 @@
 // This file will add both p5 instanced and global intellisence 
-import * as p5Global from 'p5/global' 
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as p5Global from 'p5/global'
 import module = require('p5');
 export = module;
 export as namespace p5;
