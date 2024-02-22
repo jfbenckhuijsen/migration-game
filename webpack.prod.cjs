a24422d21517f1edfccc6169ba7096142b627a20
Jeroen Benckhuijsen <jeroen.benckhuijsen@group9.nl>
2024-02-22 22:39:44 +0100

    Fix minifcation and production budgets

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -4,4 +4,9 @@ const common = require('./webpack.common.cjs');
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
+    performance: {
+        hints: false,
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1500000,
+    }
 });
